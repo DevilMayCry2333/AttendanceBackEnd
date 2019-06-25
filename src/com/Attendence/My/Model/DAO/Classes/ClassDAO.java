15bb15539ea1c29b79004317bd6d3cd3008dc8b4
DevilMayCry2333 <1712363499@qq.com>
2019-06-25 11:40:04 +0800

    最新

--- src/com/Attendence/My/Model/DAO/Classes/ClassDAO.java
+++ src/com/Attendence/My/Model/DAO/Classes/ClassDAO.java
@@ -21,30 +21,6 @@ public class ClassDAO {
         }
         return re;
     }
-<<<<<<< Updated upstream
-=======
-    public boolean InsertClass(ArrayList<String> al){
-        DBUtils db=new DBUtils();
-        Connection conn=db.getConnecton();
-        String st="INSERT INTO Classes vaues(";
-        for (int i = 0; i < al.size(); i++) {
-            if(i == al.size()-1) {
-                String tmp = al.get(i);
-                st = st + tmp + ",";
-            }else {
-                String tmp = al.get(i);
-                st = st + tmp;
-            }
-        }
-        st = st + ")";
-//        System.out.println(st);
-//        return true;
-
-        boolean c = false;
-        try {
-            Statement stt=conn.createStatement();
-            c=stt.execute(st);
->>>>>>> Stashed changes
 
     public boolean InsertClass(ArrayList<String> list) throws SQLException {
 //        String sql="INSERT INTO Classes(ClassId,Cname,Mtime,Atime)values('" + list.get(0) + "','"
@@ -67,9 +43,9 @@ public class ClassDAO {
 //        }
 
 
-        DBUtils dbUtils = new DBUtils();
-        Connection con = dbUtils.getConnecton();
-        String sql = "insert into Classes value (null,?,?,?,?)";
+        DBUtils dbUtils=new DBUtils();
+        Connection con= dbUtils.getConnecton();
+        String sql = "insert into Classes(ClassId, Cname, Mtime, Atime)  value (?,?,?,?)";
         PreparedStatement pstmt = null;
         pstmt = con.prepareStatement(sql);
         pstmt.setString(1, list.get(0));
@@ -109,46 +85,5 @@ public class ClassDAO {
         return true;
 
     }
-    public boolean ClassUpdate(ClassUpdate classUpdate){
-    String sql="UPDATE employ set(ClassID='" + classUpdate.getClassId() + "', Cname='"+ classUpdate.getCname()+"',Mtime'"+classUpdate.getMtime()+"',Atime'"+classUpdate.getAtime()+"')";
-    DBUtils dbUtils=new DBUtils();
-    Connection con=dbUtils.getConnecton();
-    //boolean c=false;
-                try {
-        Statement st=con.createStatement();
-        int result=st.executeUpdate(sql);
-        if(result>0){
-            return true;
 
-        }
-    }catch (Exception e){
-        e.printStackTrace();
-    }
-                return false;
-}
-
-    public boolean UpdateClass(ArrayList<String> list) throws SQLException {
-        DBUtils dbUtils=new DBUtils();
-        Connection con= dbUtils.getConnecton();
-
-        String sql = "update Classes set ClassId=?,Cname=?,Mtime=?,Atime=? where Id = ?";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-
-        pstmt.setString(1,list.get(1));
-        pstmt.setString(2,list.get(2));
-        pstmt.setString(3,list.get(3));
-        pstmt.setString(4,list.get(4));
-        pstmt.setInt(5, Integer.parseInt(list.get(0)));
-
-        int c = pstmt.executeUpdate();
-
-        if(c==1){
-            return  true;
-        }
-        else {
-            return false;
-        }
-
-
-    }
 }
