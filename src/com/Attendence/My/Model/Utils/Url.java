e4cddf8b50ce5068faba5b303e441318e2d05b16
DevilMayCry2333 <1712363499@qq.com>
2019-06-27 15:40:14 +0800

    最新

--- src/com/Attendence/My/Model/Utils/Url.java
+++ src/com/Attendence/My/Model/Utils/Url.java
@@ -1,7 +1,10 @@
 package com.Attendence.My.Model.Utils;
 
 public class Url {
-    public static String Url = "http://localhost:8080/unnamed/";
-    public static String FUrl = "http://localhost/AttendanceFrontEnd/";
+//    public static String Url = "http://localhost:8080/unnamed/";
+//    public static String FUrl = "http://localhost/AttendanceFrontEnd/";
+
+    public static String Url = "https://youkaiyu.com:8445/unnamed/";
+    public static String FUrl = "https://youkaiyu.com/AttendanceFrontEnd/";
 
 }
