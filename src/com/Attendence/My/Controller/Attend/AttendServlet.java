eb0e151120f6c75f948a6eb76661ef6e3ceb4310
Your Name <you@example.com>
2019-07-05 18:49:28 +0800

    最新

--- src/com/Attendence/My/Controller/Attend/AttendServlet.java
+++ src/com/Attendence/My/Controller/Attend/AttendServlet.java
@@ -22,7 +22,7 @@ public class AttendServlet extends HttpServlet {
         response.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Credentisaals", "true");
         response.setContentType("text/html");
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
